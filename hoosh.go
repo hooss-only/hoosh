d5d4470aa765f56c422e72f4bc964cda45c88890
hooss-only <hoosshusu@gmail.com>
2021-07-12 15:41:19 +0900

    bug fix

--- hoosh.go
+++ hoosh.go
@@ -12,20 +12,21 @@ import (
 
 func Encode(text string) string {
 	texts := strings.Split("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890`~-_=+]}[{\\|'\"?.>,</;:!@#$%^&*() ", "")
-	numbers := stack.Stack{}
-	hooshed := 0
-	for i := range texts {
+	numbers := stack.Stack{} // 텍스트별 대치 숫자
+	hooshed := 0             // 결과값 난독화 전
+	for i := range texts {   // 텍스트별 대치 숫자 추가
 		rand.Seed(int64(i))
 		numbers.Push(strconv.Itoa(rand.Int()))
 	}
 	for index, i := range texts {
 		if strings.Contains(text, i) {
+
 			f, err := strconv.Atoi(numbers.GetStack()[index])
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(2)
 			}
-			hooshed += f
+			hooshed += f * strings.Count(text, i)
 		}
 	}
 	hooshNumber := strings.Split("1234567890", "")
